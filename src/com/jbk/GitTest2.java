54b6cd23617f027f83002bf2da0e8df89c3f9e85
Anonymous <Anonymous@192.168.1.6>
2020-01-15 17:51:58 +0530

    this is team commit

---
+++ src/com/jbk/GitTest2.java
@@ -0,0 +1,15 @@
+package com.jbk;
+
+public class GitTest2 {
+	public static void main(String[] args) {
+		String work="done";
+		if(work == "done"){
+			System.out.println("You can go home...");
+		}else{
+			System.out.println("Complete the work...");
+		}
+		
+	
+	}
+
+}
